9637099074b992148bac9615c2c6296d1443e19e
cinbit <saintyang@hotmail.com>
2015-02-19 18:19:03 +0800

    20150219-1
    change

--- javascripts/datafriends.js
+++ javascripts/datafriends.js
@@ -12,3 +12,4 @@ friends[3] = {name:"Btc123导航",url:"http://www.btc123.com/"};
 friends[4] = {name:"比特币实时K线行情",url:"http://k.sosobtc.com/"};
 friends[5] = {name:"8btc论坛",url:"http://8btc.com/"};
 friends[6] = {name:"比特帮",url:"http://www.btcside.com/"};
+friends[7] = {name:"钱包啦",url:"http://wallet.la/"};
