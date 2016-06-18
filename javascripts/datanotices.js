b8014ffbe1c41078b83b58920019261a961b0b3e
cinbit <saintyang@hotmail.com>
2016-06-18 14:28:44 +0800

    20160618

--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -6,7 +6,4 @@
 
 //The maximum number of words is 56, decrease 3 by add each line.
 var news = new Array();
-news[0] = {text:"长期寻合作伙伴",url:"http://www.weibo.com/u/5399893323"};
-news[1] = {text:"新增8项免费比特币资源",url:"http://www.weibo.com/u/5399893323"};
-news[2] = {text:"狗狗币抢红包啦",url:"http://www.weibo.com/u/5399893323"};
-news[3] = {text:"适合新手的比特币钱包请联系本站进行推广",url:"mail:contact@cinbit.com"};
+news[0] = {text:"网站打不开或者不能正常获取比特币需使用VPN",url:"https://www.baidu.com/s?wd=%E5%85%8D%E8%B4%B9vpn&rsv_spt=1&rsv_iqid=0xd6a3a9a900199fd3&issp=1&f=3&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=1&rsv_sug1=1&rsv_sug2=1&prefixsug=%E5%85%8D%E8%B4%B9&rsp=1&inputT=5805&rsv_sug4=5806&jmp=jsver&_vr=0.5820377224590629"};
