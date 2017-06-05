f13d1a85161c82fb734a1a6991a74c6129766109
cinbit <saintyang@hotmail.com>
2017-06-05 15:49:39 +0800

    change friendlinks
    updata

--- javascripts/datafriends.js
+++ javascripts/datafriends.js
@@ -7,13 +7,7 @@
 var friends = new Array();
 friends[0] = {name:"比特币官网",url:"https://bitcoin.org/zh_CN/"};
 friends[1] = {name:"比特币维基百科",url:"https://zh-cn.bitcoin.it/wiki/%E9%A6%96%E9%A1%B5"};
-friends[2] = {name:"比特币创世论文",url:"http://www.btc123.com/data/docs/bitcoin_cn.pdf"};
-friends[3] = {name:"Btc123导航",url:"http://www.btc123.com/"};
+friends[2] = {name:"比特币创世论文",url:"https://bitcoin.org/bitcoin.pdf"};
+friends[3] = {name:"比特币创世论文(翻译）",url:"http://www.8btc.com/wiki/bitcoin-a-peer-to-peer-electronic-cash-system"};
 friends[4] = {name:"比特币实时K线行情",url:"http://k.sosobtc.com/"};
 friends[5] = {name:"8btc论坛",url:"http://8btc.com/"};
-friends[6] = {name:"钱包啦",url:"http://wallet.la/"};
-friends[7] = {name:"币推",url:"https://bitui.io/"};
-friends[8] = {name:"时针角度计算",url:"calangle.html"};
-friends[9] = {name:"歪啦象棋演示",url:"chinesechess.html"};
-friends[10] = {name:"Xapo注册教程",url:"http://www.cinbit.com/tutorial.html?t=0&h=0"};
-friends[11] = {name:"免费比特币FarmBitcoin游戏教程",url:"noframefiles/farmbitcoin.html"};
