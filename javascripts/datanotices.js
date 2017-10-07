7756f39d7cc27bfd44a1d33f31f538a6c887270c
cinbit <saintyang@hotmail.com>
2017-10-08 06:57:46 +0800

    add notice
    20171008

--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -7,5 +7,5 @@
 //The maximum number of words is 56, decrease 3 by add each line.
 var news = new Array();
 news[0] = {text:"微钱包faucethub注册帮助",url:"http://www.cinbit.com/tutorial.html?t=0&h=0"};
-news[1] = {text:"免费获取比特币新手必读",url:"http://www.weibo.com/ttarticle/p/show?id=2309404115283504671827"};
-news[2] = {text:"中国比特币海外版交易所上线啦",url:"https://www.exx.com/r/ef668fb591c021e8c12c744e9558439e"};
+news[1] = {text:"显示谷歌reCAPTCHA验证码方法",url:"http://ggfwzs.com"};
+news[2] = {text:"中国比特币海外版交易所上线",url:"https://www.exx.com/r/ef668fb591c021e8c12c744e9558439e"};
