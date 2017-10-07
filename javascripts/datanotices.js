5cf37a7c9cbc09a81bc384d00382ec7b8efc9a45
cinbit <saintyang@hotmail.com>
2017-10-07 12:10:46 +0800

    add exx
    20171007

--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -8,3 +8,4 @@
 var news = new Array();
 news[0] = {text:"微钱包faucethub注册帮助",url:"http://www.cinbit.com/tutorial.html?t=0&h=0"};
 news[1] = {text:"免费获取比特币新手必读",url:"http://www.weibo.com/ttarticle/p/show?id=2309404115283504671827"};
+news[2] = {text:"中国比特币海外版交易所上线啦",url:"https://www.exx.com/r/ef668fb591c021e8c12c744e9558439e"};
