34e16c61122d2f33d3759a3f92d3c7b008726cba
cinbit <saintyang@hotmail.com>
2017-10-08 22:05:39 +0800

    add freebitco
    20171008

--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -9,3 +9,4 @@ var news = new Array();
 news[0] = {text:"微钱包faucethub注册帮助",url:"http://www.cinbit.com/tutorial.html?t=0&h=0"};
 news[1] = {text:"显示谷歌reCAPTCHA验证码方法",url:"http://ggfwzs.com"};
 news[2] = {text:"中国比特币海外版交易所上线",url:"https://www.exx.com/r/ef668fb591c021e8c12c744e9558439e"};
+news[3] = {text:"推荐观看",url:"http://weibo.com/ttarticle/p/show?id=2309404160546671823072"};
