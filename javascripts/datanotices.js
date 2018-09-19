06635444bbf24b77e990f09c50e07fff65dc4081
cinbit <cinbit@users.noreply.github.com>
2018-09-20 01:16:19 +0800

    update
    change notice

--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -6,7 +6,7 @@
 
 //The maximum number of words is 56, decrease 3 by add each line.
 var news = new Array();
-news[0] = {text:"微钱包faucethub注册帮助",url:"http://www.cinbit.com/tutorial.html?t=0&h=0"};
-news[1] = {text:"本站开发了显示谷歌reCAPTCHA验证码的插件",url:"http://www.cinbit.com/recaptcha_show_1.0.0.crx"};
-news[2] = {text:"中国比特币海外版交易所上线",url:"https://www.exx.com/r/ef668fb591c021e8c12c744e9558439e"};
-news[3] = {text:"推荐观看",url:"http://weibo.com/ttarticle/p/show?id=2309404163008019480935"};
+news[0] = {text:"FH钱包注册帮助",url:"http://t.cn/EvJsmHN"};
+news[1] = {text:"CM矿机软件下载",url:"https://openload.co/f/UG0rsvnmD9Y"};
+news[2] = {text:"谷歌reCAPTCHA验证码插件下载",url:"http://www.cinbit.com/recaptcha_show_1.0.0.crx"};
+news[3] = {text:"誉通矿池，给您超级简单的挖矿体验！",url:"http://cinbit.com"};
