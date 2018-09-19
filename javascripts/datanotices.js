46450a807142c5440abcbb2b2a2282e283e6d47e
cinbit <cinbit@users.noreply.github.com>
2018-09-20 02:08:10 +0800

    update
    change notice

--- javascripts/datanotices.js
+++ javascripts/datanotices.js
@@ -6,7 +6,6 @@
 
 //The maximum number of words is 56, decrease 3 by add each line.
 var news = new Array();
-news[0] = {text:"FH钱包注册帮助",url:"http://t.cn/EvJsmHN"};
-news[1] = {text:"CM矿机软件下载",url:"https://openload.co/f/UG0rsvnmD9Y"};
-news[2] = {text:"谷歌reCAPTCHA验证码插件下载",url:"http://www.cinbit.com/recaptcha_show_1.0.0.crx"};
-news[3] = {text:"誉通矿池，给您超级简单的挖矿体验！",url:"http://cinbit.com"};
+news[0] = {text:"谷歌reCAPTCHA验证码插件下载",url:"http://www.cinbit.com/recaptcha_show_1.0.0.crx"};
+news[1] = {text:"FH钱包注册帮助",url:"http://t.cn/EvJsmHN"};
+news[2] = {text:"誉通矿池，给您超级简单的挖矿体验！",url:"http://cinbit.com"};
